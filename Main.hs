d239325cb35fdb160d1ebe979d69b89aaf0b8078
PROMETHIA-27 <42193387+PROMETHIA-27@users.noreply.github.com>
2021-09-30 12:52:01 -0400

    Added new file for rough draft

--- Main.hs
+++ Main.hs
@@ -1,6 +1,4 @@
-module Main where
-
-type Order = [String]
+import Data.List
 
 remove _ [] = []
 remove x (y:ys) | x == y = remove x ys
@@ -14,34 +12,31 @@ printAll (x:xs) = do
 	printAll(xs)
 printAll [] = putStrLn("No characters in turn order")
 
-simulate :: Order -> IO ()
 simulate ord = do
 	input <- getLine
 	act input ord
 
-act :: String -> Order -> IO ()
-act ('A':'d':'d':' ':name) ord = do
-	putStrLn("Adding " ++ name)
-	simulate (name:ord)
-act ('R':'e':'m':'o':'v':'e':' ':name) ord = do
-	putStrLn("Removing " ++ name)
-	simulate (remove name ord)
-act "Order" ord = do 
-	printAll(ord)
-	simulate ord
-act "Turn" ord = do
-	if length ord > 0 
-		then do
-			let first = ord !! 0
-			putStrLn(first)
-			simulate ((pop ord) ++ [first])
-		else do
-			putStrLn("No characters added! It is no one's turn.")
-			simulate ord
-act "Exit" ord = putStrLn("See you later!")
-act _ ord = do
-	putStrLn("Please enter a valid input. Valid commands: Add [Name], Remove [Name], Order, Turn, Exit.")
-	simulate ord
+act input ord | Just name <- stripPrefix "Add " input = do
+			    putStrLn("Adding " ++ name)
+			    simulate (name:ord)
+			  | Just name <- stripPrefix "Remove " input = do
+			    putStrLn("Removing " ++ name)
+			    simulate (remove name ord)
+			  | input == "Order" = do
+			    printAll(ord)
+			    simulate ord
+			  | input == "Turn" = do
+              if length ord > 0
+                then do
+                    let first = ord !! 0
+                    putStrLn(first)
+                    simulate((pop ord) ++ [first])
+                else do
+                    putStrLn("No characters available!")
+                    simulate ord
+			  | input == "Exit" = putStrLn("See you later!")
+			  | otherwise = do
+			    putStrLn("Please enter a valid input.")
+			    simulate ord	
 
-main :: IO ()
 main = simulate []
